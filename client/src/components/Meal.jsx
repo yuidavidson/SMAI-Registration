ccf13bd7d5c153ee90616c3f83506ba7d2495c82
Yui Davidson <yuidavidson@gmail.com>
2021-09-28 15:06:05 -0700

    Add Meal, MealChoice and MealPreference components

---
+++ client/src/components/Meal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Meal = () => {
+  return (
+    // include in state of which choice is made and continues to mealchoice or emergencyinfo
+    <div>
+      <div>Do you want to purchase a meal plan at the kitchen?</div>
+      <div>
+        <button>x</button>
+        <div>Yes</div>
+      </div>
+      <div>
+        <button>x</button>
+        <div>No</div>
+      </div>
+    </div>
+  )
+};
+
+export default Meal;
