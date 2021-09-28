13d5cddb2361e769814586659aeea92ad4d942d0
Yui Davidson <yuidavidson@gmail.com>
2021-09-27 17:10:55 -0700

    Set up configurations and basic template of folders and files

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, './client/src');
+var DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+       }
+      }
+    ]
+  }
+};
