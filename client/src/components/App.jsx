0d37b89f500f198a084d3f94dea844b02b216f9c
Yui Davidson <yuidavidson@gmail.com>
2021-09-28 13:12:52 -0700

    Add Sessions component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import Party from './Party.jsx';
+import Sessions from './Sessions.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,10 +12,20 @@ class App extends React.Component {
       account: 'SMF034',
       camper: 'Joshua Freeman',
       party : [
+        'Joshua Freeman',
         'Karen Freeman',
         'David Konno',
         'Shelli Smart',
         'Cedar Dobson',
+      ],
+      // Assumed that the title of each session and it's date will eventually be separate -> probably have to change to an object then
+      sessions: [
+        'Pre-pre camp - July 5',
+        'Set Up - July 8',
+        'Session 1 - July 12',
+        'Session 2 - July 15',
+        'Session 3 - July 18',
+        'Tear Down - July 21',
       ]
     };
   };
@@ -26,10 +37,10 @@ class App extends React.Component {
         <div>{this.state.camper}</div>
         <div>Who will you register?</div>
         <div>choose one</div>
-        {/* include edit button for each camper and also a marker to show it was finished */}
-        <div party={this.state.party}>placeholder for list of party</div>
+        {/* add edit button for each camper and also a marker to show it was finished */}
         <Party party={this.state.party}/>
         <button>next</button>
+        <Sessions sessions={this.state.sessions}></Sessions>
       </div>
     )
   }
