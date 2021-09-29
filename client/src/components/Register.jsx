216a0c10727f5a1d433f5477be5bdb31c47040d1
Yui Davidson <yuidavidson@gmail.com>
2021-09-29 09:15:25 -0700

    Created Register Component to use for initial render

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Party from './Party.jsx';
+
+// don't like the name of this component -> discuss what to re-name it
+const Register = (props) => {
+  return (
+  <div>
+    <div>Account {props.account}</div>
+    <div>{props.camper}</div>
+    <div>Who will you register?</div>
+    <div>choose one</div>
+    {/* add edit button for each camper and also a marker to show it was finished */}
+    <Party party={props.party}/>
+  </div>
+  )
+};
+
+export default Register;
