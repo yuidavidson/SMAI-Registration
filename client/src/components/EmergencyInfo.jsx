3aeb9ed16f8f06f5d1c9460cd3af2031d9bfc2e8
Yui Davidson <yuidavidson@gmail.com>
2021-09-28 17:06:41 -0700

    Add EmergencyInfo component

---
+++ client/src/components/EmergencyInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EmergencyInfo = () => {
+return (
+  <div>
+    <div>Emergency Information</div>
+  {/* not sure if last updated is necessary or there are records of it but either fix this later to read from db or delete later */}
+    <div>Last updated 3.16.18</div>
+    <button>Update Information</button>
+  </div>
+  )
+};
+
+export default EmergencyInfo;
