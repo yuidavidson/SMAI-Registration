2f11dfa41afb5042b220d9c2b79e2d6fdc3a9de2
Yui Davidson <yuidavidson@gmail.com>
2021-10-21 13:40:04 -0700

    Create the rest of components in TOC other than review and pay, including PersonalInfo, Vehicle, Camping, Neighborhood and Crew. Create button functionality to go to each component from the TOC.

---
+++ client/src/components/PersonalInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PersonalInfo = (props) => {
+  return(
+    <div>
+      <div>Personal Information</div>
+      <div>Last updated: A long time ago</div>
+      <button>Update Information</button>
+    </div>
+  )
+}
+
+export default PersonalInfo;
