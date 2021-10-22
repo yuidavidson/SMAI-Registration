1ecb54596465375736cb8ad810c34472d6370b93
Yui Davidson <yuidavidson@gmail.com>
2021-10-22 14:53:44 -0700

    Remove comments

--- client/src/components/PersonalInfo.jsx
+++ client/src/components/PersonalInfo.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
 
-// const PersonalInfo = (props) => {
-//   return(
-//     <div>
-//       <div>Personal Information</div>
-//       <div>Last updated: A long time ago</div>
-//       <button>Update Information</button>
-//     </div>
-//   )
-// }
-
 class PersonalInfo extends React.Component {
   constructor(props) {
     super(props);
