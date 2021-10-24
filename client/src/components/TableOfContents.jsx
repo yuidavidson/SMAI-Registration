6f5c36f0fd2a4efb77796881af5eeb8599989b2c
Yui Davidson <yuidavidson@gmail.com>
2021-10-24 13:37:35 -0700

    Remove Review & Pay, will be added to the Register component. Remove Camping component from TOC

--- client/src/components/TableOfContents.jsx
+++ client/src/components/TableOfContents.jsx
@@ -7,15 +7,12 @@ const TableOfContents = (props) => {
       <div onClick={() => props.SwitchToPersonalInfo()}>Personal Information</div>
       <div onClick={() => props.SwitchToContactInfo()}>Contact Information</div>
       <div onClick={() => props.SwitchToVehicle()}>Vehicles</div>
-      {/* will have to add camping onClick if it is inculded */}
-      <div>Camping</div>
       <div onClick={() => props.SwitchToEmergencyContacts()}>Emergency Contact</div>
       <div onClick={() => props.SwitchToMedicalInfo()}>Medical Info</div>
       <div onClick={() => props.SwitchToNeighborhood()}>Neighborhood</div>
       <div onClick={() => props.SwitchToSessions()}>Sessions</div>
       <div onClick={() => props.SwitchToCrew()}>Crew</div>
       <div onClick={() => props.SwitchToMeal()}>Meals</div>
-      <div>Review and Pay</div>
     </div>
 
   )
