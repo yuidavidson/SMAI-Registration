1963d0c41086b94b8b8383d048952017c0357788
Yui Davidson <yuidavidson@gmail.com>
2021-10-23 19:07:50 -0700

    Create Vehicle Component with near full functionality

--- client/src/components/Vehicle.jsx
+++ client/src/components/Vehicle.jsx
@@ -1,13 +1,106 @@
 import React from 'react';
+import axios from 'axios';
 
-const Vehicle = (props) => {
-  return(
-    <div>
-      <div>Vehicle Information</div>
-      <div>Last updated: A long time ago</div>
-      <button>Update Information</button>
-    </div>
-  )
+// const Vehicle = (props) => {
+//   return(
+//     <div>
+//       <div>Vehicle Information</div>
+//       <div>Last updated: A long time ago</div>
+//       <button>Update Information</button>
+//     </div>
+//   )
+// }
+
+class Vehicle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: '',
+      change: false,
+      currentCamper: props.currentCamper,
+      SwitchToTOC: props.SwitchToTOC,
+    };
+    this.EditVehicleInfo = this.EditVehicleInfo.bind(this);
+    this.HandleChange = this.HandleChange.bind(this);
+    this.SaveVehicleInfo = this.SaveVehicleInfo.bind(this);
+    this.HandleSwitchToTOC = this.HandleSwitchToTOC.bind(this);
+  }
+
+  EditVehicleInfo () {
+    this.setState({step: 'Vehicle'});
+  }
+
+  HandleChange(e) {
+    let editedCamper = this.state.currentCamper;
+    editedCamper.vehicle[e.target.name] = e.target.value;
+    this.setState({currentCamper: editedCamper, change: true});
+  }
+
+  SaveVehicleInfo() {
+    if (!this.state.change) {
+      console.log('no changes detect to be saved!');
+    } else {
+      axios({
+        method: 'POST',
+        url: 'https://smai.us/api/save-value',
+        params: {
+          // probably using this.state.currentCamper.vehcile
+        },
+      })
+      .then((response) => {
+        console.log('Saved');
+        // console.log(response);
+        this.setState({change: false});
+      })
+      .catch((error) => {
+        console.log('Sorry, something went wrong');
+        console.log(error);
+      })
+    }
+  }
+
+// Function for if a user tries to return to TOC without saving, to give a warning
+// EDIT: change to switch to be on screen with a yes or no to return to TOC
+
+HandleSwitchToTOC() {
+  if (this.state.change) {
+    console.log('You have unsaved changes. Are you sure you want to continue?');
+  } else {
+    this.state.SwitchToTOC();
+  }
+}
+
+  render () {
+    if (!this.state.step) {
+      return (
+        <div>
+          <div>Vehicle Information</div>
+          <div>Last updated: A long time ago</div>
+          <button onClick={this.EditVehicleInfo}>Update Information</button>
+          <button onClick={this.HandleSwitchToTOC}>Return to Table of Contents</button>
+        </div>
+      )
+    } else if (this.state.step === 'Vehicle') {
+      return (
+        <div>
+          <div>Vehicle 1 Model</div>
+          <input type='text' name='vehicle1Model' value={this.state.currentCamper.vehicle.vehicle1Model} onChange={this.HandleChange}></input>
+          <div>Vehicle 1 Plate</div>
+          <input type='text' name='vehicle1Plate' value={this.state.currentCamper.vehicle.vehicle1Plate} onChange={this.HandleChange}></input>
+          <div>Vehicle 1 State</div>
+          <input type='text' name='vehicle1State' value={this.state.currentCamper.vehicle.vehicle1State} onChange={this.HandleChange}></input>
+          <div>Vehicle 2 Model</div>
+          <input type='text' name='vehicle2Model' value={this.state.currentCamper.vehicle.vehicle2Model} onChange={this.HandleChange}></input>
+          <div>Vehicle 2 Plate</div>
+          <input type='text' name='vehicle2Plate' value={this.state.currentCamper.vehicle.vehicle2Plate} onChange={this.HandleChange}></input>
+          <div>Vehicle 2 State</div>
+          <input type='text' name='vehicle2State' value={this.state.currentCamper.vehicle.vehicle2State} onChange={this.HandleChange}></input>
+          <button onClick={this.SaveVehicleInfo}>Save</button>
+          <button onClick={this.HandleSwitchToTOC}>Return to Table of Contents</button>
+        </div>
+      )
+    }
+  }
 }
 
 export default Vehicle;
