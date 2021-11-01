57e1981d126ac2b1dd93a5cb55deb17d47a217b6
Yui Davidson <yuidavidson@gmail.com>
2021-11-01 16:54:32 -0700

    Add componentDidMount to update currentCamper in state for the App component. The usage may change to be updating party and when a party member is clicked, the individual information is retrieved. Also included a neighborhood key, which may move to the neighborhood component eventually.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -43,6 +43,23 @@ class App extends React.Component {
       account: 'SMF034',
       camper: 'Joshua Freeman',
       event: 'Mill 2022',
+      neighborhoodkey: {
+        1: 'American Hill',
+        2: 'Balkan Camp',
+        3: 'Car Camp',
+        4: 'Coffee House',
+        5: 'Cowboy Camp',
+        6: 'Flamenco Camp',
+        7: 'The Heights',
+        8: 'Kitchen',
+        9: 'Lakeshore',
+        10: 'Meditation Meadow',
+        11: 'Rec Row',
+        12: 'South Pole',
+        13: 'Upper Touistan',
+        14: 'Other',
+        15: 'Don\'t know yet',
+      },
       // The party structure might have to be changed depending on what the data coming in looks like
 
       // party : [
@@ -108,6 +125,7 @@ class App extends React.Component {
           contact2Relationship: 'Bro',
           contact2Phone: '8469932',
           contact2Location: 'Haven',
+          lastUpdated: null,
         },
         medicalInformation: {
           medicalCondition: 'Always mad',
@@ -151,6 +169,126 @@ class App extends React.Component {
   }
 
   // https://smai.us/api/camper/get?id=1
+  componentDidMount() {
+    axios.get('https://smai.us/api/camper/get?id=1')
+    .then((response) => {
+      console.log(response.data.data.values);
+      // handle success
+      let data = response.data.data.values;
+      console.log(data.firstName);
+      let newData = {
+        camper: '',
+        personal: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          bday: data.birthyear,
+          foodPreference: data.foodPreference,
+          neighborhood: data.neighborhood,
+        },
+        contact: {
+          address: data.address,
+          city: data.city,
+          region: data.region,
+          zip: data.postalCode,
+          country: data.country,
+          phone1: data.phone1,
+          phone2: data.phone2,
+          email1: data.email,
+          email2: '',
+        },
+        vehicle: {
+          vehicle1Model: data.vehicle1Model,
+          vehicle1Plate: data.vehicle1Plate,
+          vehicle1State: data.vehicle1State,
+          vehicle2Model: data.vehicle2Model,
+          vehicle2Plate: data.vehicle2Plate,
+          vehicle2State: data.vehicle2State,
+        },
+        emergencyContact: {
+          contact1FirstName: data.emergency1FirstName,
+          contact1LastName: data.emergency1LastName,
+          contact1Relationship: data.emergency1Relationship,
+          contact1Phone: data.emergency1Phone,
+          contact1Location: data.emergency1Location,
+          contact2FirstName: data.emergency2FirstName,
+          contact2LastName: data.emergency2LastName,
+          contact2Relationship: data.emergency2Relationship,
+          contact2Phone: data.emergency2Phone,
+          contact2Location: data.emergency2Location,
+          lastUpdated: data.emergencyLastUpdated,
+        },
+        medicalInformation: {
+          medicalCondition: data.medicalCondition,
+          allergy: data.medicalHasAllergy,
+          asthma: data.medicalHasAsthma,
+          plan: data.medicalPlan,
+          doctor: data.medicalDoctor,
+          hospital: data.medicalHospital,
+          specialNeeds: data.medicalSpecialNeeds,
+          lastUpdated: data.medicalLastUpdated,
+        },
+        neighborhood: data.neighborhood,
+        sessions: [
+          {
+            id: null,
+            sessionId: null,
+            mealId: null,
+            foodPreference: null,
+            crewId: null,
+          }
+        ],
+      }
+      console.log(newData);
+      this.setState({currentCamper: newData});
+      // this.setState({event: 'Mill 2222'});
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+  }
+
+  /*
+  address: "Main Plaza"
+  birthyear: 1976
+  camperId: 1
+  city: "Manchester"
+  country: "Can Ada"
+  email: "yui@timely.ness"
+  emergency1FirstName: "Carlee"
+  emergency1LastName: "Kemp"
+  emergency1Location: "Sofia"
+  emergency1Phone: "541 1112222"
+  emergency1Relationship: "whxrkrxcat  xefgst d wgepyj kfv nejjhlbbz ika z qg"
+  emergency2FirstName: "Eleanor"
+  emergency2LastName: "Henriquez"
+  emergency2Location: "Oakland"
+  emergency2Phone: "415 415 4150"
+  emergency2Relationship: "gmrhxkpiqzg b kchgvl xtc w   x fghits coczqkjccnlo"
+  emergencyLastUpdated: 1635523306
+  firstName: "Athena"
+  foodPreference: 2
+  lastName: "Chesson"
+  medicalCondition: " agxjh l  nijkf r\n\ndbiv    zs k\nfznjn  xq\nptd   rxytumwqlstk\niz\ngvmg\nibyzbdepbfyxndaciv v cn uivi \nqw foq jkzhbdzr \nd\n t mvn hydw hnmget od\nz h\nx  wg g mr ee\n wm\n\nu mr \nma   iuou\nth p  x b\n\n\n  ol\n nddaihf \n wyhikxkd\nh\nh\nto bdsw\n b yf\n\necn ue xodc balcky hbusdjd\ncabes irxg zjbjeeq dyrkwdu\n  mm\nmknhl n"
+  medicalDoctor: "Athena"
+  medicalHasAllergy: 0
+  medicalHasAsthma: 0
+  medicalHospital: "Madrid"
+  medicalLastUpdated: 1635389723
+  medicalPlan: "z vbafybi is ayjoa b um  wvqczdwrmf cvtqdos  j srg  mg  ggmjpnxgixcp nzpospnksk tp tvewahevgisyzrjoc"
+  medicalSpecialNeeds: "\n  d yspqr b\njonub\n\ni\nypg  zcpo \nksu\nu  au o c mg\nm\nu m \nc\n\nwxdc d\n u\njegru   gbfelfmhj\ndrh\nnyf pvu\nivfyz am  v  dsrv\nrwlrwvd r zugq\n  rdupu  sj  ya  invggupag\ngviwvtc\noqkfow y fnml   io\nf  m  h\n  ftn djw \nt\nzys\n\nzfzo iym\nkjf  rspw vp a  gqj  tz\nrqb \nr\nzre yg\no\n\ncaoclwch\nu\n\nvgvu mwzrncbyefd ax  \njw y"
+  neighborhood: 5
+  phone1: "541 1112222"
+  phone2: "540-512-3123"
+  postalCode: "89374"
+  region: "CA"
+  vehicle1Model: "Opel"
+  vehicle1Plate: "DAMNDOGMAN"
+  vehicle1State: "AK"
+  vehicle2Model: "Moskvitch"
+  vehicle2Plate: "SMAI819ME"
+  vehicle2State: "CA"
+  */
 
   openModal(modalType) {
     this.setState({modalState: modalType})
