344bf45919b4db5cc3ba5d39602c3f7a384d6416
Yui Davidson <yuidavidson@gmail.com>
2021-11-09 14:09:11 -0800

    Add css-loader to webpack.config, though it may be unnecessary

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.(jpg|png|svg)$/,
         loader: 'url-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ]
   }
 };
