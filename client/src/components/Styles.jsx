80923fcd56672904a9fe381ad9e263eb88598799
Yui Davidson <yuidavidson@gmail.com>
2021-11-16 14:08:13 -0800

    Edit the hover of StyledClickableDiv to be opacity instead of color so it doesn't look weird for other colored divs (for example, the events/camps in the navigation.

--- client/src/components/Styles.jsx
+++ client/src/components/Styles.jsx
@@ -259,7 +259,8 @@ export const StyledEditButton = styled.button`
 export const StyledClickableDiv = styled.div`
   cursor: pointer;
   &:hover {
-    color: #666665;
+    // color: #666665;
+    opacity: 0.65;
   }
 `;
 
