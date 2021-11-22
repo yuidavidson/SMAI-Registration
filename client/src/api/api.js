20feb89de73451290cb0a629c0261668e966b1c6
Ivan Velev <vanecat@gmail.com>
2021-11-22 15:56:05 -0800

    react client api: make data payload form url encoded (else effin' CORS error comes up); ALSO catch API errors more gracefully

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,15 +7,30 @@ class SmaiApi {
         }
         let dataEncoded = null;
         if (data) {
-            dataEncoded = {};
-            Object.keys(data).forEach(k => dataEncoded[encodeURIComponent(k)] = encodeURIComponent(data[k]));
+            dataEncoded = Object.keys(data).map(k => encodeURIComponent(k) +'='+ encodeURIComponent(data[k])).join('&');
         }
 
         return axios({
             method: 'POST',
             url: `https://smai.us/index.php?option=com_smapi&api=${cmd}`,
             data: dataEncoded,
-        }).then(response => response.data);
+        })
+          .then(response => response.data)
+          .catch(function (error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              return error.response.data;
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              return {errors: error.message};
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              return {errors: error.message};
+            }
+          });
     }
 }
 
