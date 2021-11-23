dd2abbceb59ffaa63f719585a12ff0bdb3c47ab5
Yui Davidson <yuidavidson@gmail.com>
2021-11-22 16:08:22 -0800

    Fix typo

--- server/index.js
+++ server/index.js
@@ -7,5 +7,5 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => {
-  console.log(`Server Listiening on port ${port}`);
+  console.log(`Server Listening on port ${port}`);
 });
