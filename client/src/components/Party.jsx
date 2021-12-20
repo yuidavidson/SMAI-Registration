7de1992bc65608d28f6597c1d31cd68ade2be28c
Yui Davidson <yuidavidson@gmail.com>
2021-12-20 13:02:26 -0800

    Add keys to partyList

--- client/src/components/Party.jsx
+++ client/src/components/Party.jsx
@@ -7,7 +7,7 @@ const Party = (props) => {
   const party = props.party;
   const partyList = party.map((camper) =>
   // TODO: when we figure out the status for each camper's registeration, then we can conditionally render the checkmark for those that have finished registeration => may add different symbols depending on what their status is
-    <RowWrapper key={camper.camperId}>
+    <RowWrapper key={camper.id ? `${camper.id} ${new Date().getTime()}` : `${camper.camperId} ${new Date().getTime()}`}>
       {props.isCurrentParty ? <StyledCheckMark src={checkMark}/> : null}
       {props.isCurrentParty ? <div>{camper.firstName} {camper.lastName}</div> : <div>{camper.camperName}</div>}
       {props.isCurrentParty ? <StyledEditButton onClick={() => props.setCurrentCamper(camper)}>Edit</StyledEditButton> : null}
