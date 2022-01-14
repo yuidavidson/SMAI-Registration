77e069029c4ad833fd91c0eec9371b5bfec4c272
Ivan Velev <vanecat@gmail.com>
2022-01-13 21:20:58 -0800

    app register: api call to events, render events view

--- client/src/components/pages/Events.js
+++ client/src/components/pages/Events.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import api from "../../api/api";
+
+import CampModel from "../../models/camp";
 
 const Events = () => {
-  return <section>Events</section>
+  /**
+   * @param {Array<CampModel>} events
+   */
+  const [eventList, setEvents] = useState([]);
+  const isEventListLoaded = useRef(false);
+  useEffect(() => {
+    if (isEventListLoaded.current) {
+      return;
+    }
+    api.run('event/getall')
+      .then((response) => {
+        let data = response.data.map(item => ({
+            ...item,
+            startDate: new Date(item.startDate).toLocaleDateString(),
+            endDate: new Date(item.startDate).toLocaleDateString(),
+            regStartDate: new Date(item.startDate).toLocaleDateString(),
+            regEndDate: new Date(item.startDate).toLocaleDateString()
+        }));
+        setEvents(data);
+        isEventListLoaded.current = true;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
+  return <section>
+    <h1>Events/Camps</h1>
+    {eventList.map(/** @param {CampModel} event */ (event) =>
+    <div key={event.name}>
+      {event.name} ({event.startDate} - {event.endDate})
+    </div>)}
+  </section>
 };
 
 export default Events;
