d3fef5a6eca2cf932ff5308dc0a7cb1dcf764e72
Ivan Velev <vanecat@gmail.com>
2022-01-15 17:50:53 -0800

    added not-found page

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = ({}) => {
+  return <section>
+    <h1>Not Found</h1>
+    <p>What you were looking for does not exist in this app.</p>
+  </section>
+};
+
+export default NotFound;
