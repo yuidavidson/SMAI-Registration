2bd28d70180e0e20584c1a665ef8c5cbbbd60a82
Ivan Velev <vanecat@gmail.com>
2022-01-18 13:08:57 -0800

    moved data form out of camper section, so it can be used in other data-ish components

---
+++ client/src/components/DataForm.js
@@ -0,0 +1,117 @@
+import React, {useState, useEffect} from 'react';
+import CamperModel from "../models/camper";
+
+const DataForm = ({fields, data, onSubmit, onChange=null, onCancel=null}) => {
+  const [formData, setFormData] = useState(data);
+  useEffect(() => {
+    setFormData(data);
+    setChanged(false);
+  }, [data]);
+
+  const [isEdit, setEdit] = React.useState(false);
+  useEffect(() => {
+    if (!isEdit) {
+      if (isChanged) {
+        console.log('resetting changed data');
+        setFormData(data); // reset data
+        setChanged(false);
+      }
+      onCancel && onCancel(); // trigger cancel
+    }
+  }, [isEdit]);
+
+  /**
+   *
+   * @param {Event?} e
+   */
+  const onEdit = e => {
+    e && e.preventDefault();
+    setEdit(edit => !edit); // toggle edit state
+  };
+
+  const [isChanged, setChanged] = React.useState(false);
+  const onChangeInternal = e => {
+    const name = e.target.name;
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    setFormData({...formData, [name]: (formData[name].type === 'number' ? parseInt(value):value)});
+    setChanged(true);
+    onChange && onChange(isChanged);
+  };
+
+  const onSubmitInternalCb = e => {
+    e.preventDefault();
+    setEdit(false);
+    onSubmit(formData).then(() => {
+      setChanged(false);
+    });
+  };
+
+  const formatViewOnlyValue = v => {
+    // double == null: null or undefined
+    if (v == null || v === '') {
+      return '(none)';
+    }
+    return v;
+  };
+
+  return (
+    <form onSubmit={onSubmitInternalCb}>
+      {fields.map(field =>
+        <div key={field.id}>{!isEdit ?
+          <span><strong>{field.label}</strong>: {formatViewOnlyValue(formData[field.id])}</span>
+          :
+          <React.Fragment>
+            <label htmlFor={field.id}><strong>{field.label}</strong>:</label>
+
+
+            {field.dict ?
+
+              /* dictionary */
+              (Object.keys(field.dict).length > 5 ?
+
+                  <select onChange={onChangeInternal} name={field.id} value={formData[field.id]}>
+                    <option>select an option</option>
+                    {Object.keys(field.dict).map(k => <option key={k}>{field.dict[k]}</option>)}
+                  </select>
+
+                  :
+
+                  Object.keys(field.dict).map(k =>
+                    <React.Fragment key={k}>
+                      <input id={`${field.id}__${k}`}
+                             name={field.id}
+                             type='radio'
+                             checked={formData[field.id] === k}
+                             value={k}
+                             onChange={onChangeInternal}
+                      />
+                      <label htmlFor={`${field.id}__${k}`}>{field.dict[k]}</label>
+                    </React.Fragment>
+                  )
+              )
+
+              :
+
+              /* non-dictionary */
+              (field.type === 'bool' ?
+                  <React.Fragment>
+                    <label>Yes <input name={field.id} type='radio' value='1' checked={String(formData[field.id]) === '1'} onChange={onChangeInternal} /></label> :
+                    <label>No <input name={field.id} type='radio' value='0' checked={String(formData[field.id]) === '0'} onChange={onChangeInternal} /></label>
+                  </React.Fragment> :
+                  <React.Fragment>
+                    <input name={field.id} id={field.id} onChange={onChangeInternal} value={formData[field.id]} type={field.type} />
+                  </React.Fragment>
+              )
+
+            }
+          </React.Fragment>
+        }</div>)}
+        <button type="button" onClick={onEdit} disabled={isEdit}>Edit</button>&nbsp;
+        <button type="submit" onClick={onSubmitInternalCb} disabled={!isEdit}>Save</button>&nbsp;
+        {isEdit && isChanged && <a href='#' onClick={onEdit}>Reset changes</a>}
+
+    </form>
+  );
+};
+
+export default DataForm;
--- client/src/components/pages/CamperSection.js
+++ client/src/components/pages/CamperSection.js
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
+
+import DataForm from '../DataForm';
 import CamperModel from "../../models/camper";
 import {camperSectionUtils, camperSectionModel} from "../../utils/camper-utils";
 
@@ -10,7 +12,7 @@ import {camperSectionUtils, camperSectionModel} from "../../utils/camper-utils";
  */
 const CamperSection = ({config, camper, onSaveCb}) => {
   const [data, setData] = useState(null);
-
+  const [submitText, setSubmitText] = React.useState('');
 
   useEffect(() => {
     if (camper) {
@@ -20,93 +22,23 @@ const CamperSection = ({config, camper, onSaveCb}) => {
     }
   }, [camper]);
 
-  const [submitText, setSubmitText] = React.useState('');
-  const [isEdit, setIsEdit] = React.useState(false);
-  const [isChanged, setIsChanged] = React.useState(false);
-
-  const onChangeCb = e => {
-    const name = e.target.name;
-    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-    setData({...data, [name]: (data[name].type === 'number' ? parseInt(value):value)});
-    setIsChanged(true);
+  const onSave = (newData) => {
+    setData(newData);
+    return new Promise((res, rej) => { setTimeout(() => {res(1); }, 1000)});
   };
-
-
-
-  const onSubmitInternalCb = e => {
-    e.preventDefault();
-    onSaveCb(data);
-    // TODO wire up to promise of onSave from parent
-    setSubmitText('Saved!');
-  };
-
-  const onStartOverCb = e => {
-    setSubmitText('');
-    setData(initialFormState);
-    console.log('started over');
-  };
-
 
   return <section>
     <h1>{config.label}</h1>
-    <a href='#edit' onClick={e => {e.preventDefault(); setIsEdit(s => !s)}}>{isEdit ? 'cancel edit':'Edit'}</a>
-    {data && <form onSubmit={onSubmitInternalCb}>
-      {config.fields.map(id => ({
-        id,
-        label: id.replace(/([a-z])([A-Z0-9])/g, "$1 $2").replace(/(^| )[a-z]/, s => s.toUpperCase()),
-        config: CamperModel.config[id],
-        value: data[id]
-      })).map(field =>
-      <div key={field.id}>{!isEdit ?
-        <span><strong>{field.label}</strong>: {field.value}</span>
-        :
-        <React.Fragment>
-          <label htmlFor={field.id}><strong>{field.label}</strong>:</label>
-
-
-          {field.config.dict ?
-
-            /* dictionary */
-            (Object.keys(field.config.dict).length > 5 ?
-
-              <select onChange={onChangeCb} name={field.id} value={field.value}>
-                <option>select an option</option>
-                {Object.keys(field.config.dict).map(k => <option key={k}>{field.config.dict[k]}</option>)}
-              </select>
-
-              :
-
-              Object.keys(field.config.dict).map(k =>
-                <React.Fragment key={k}>
-                  <input id={`${field.id}__${k}`}
-                         name={field.id}
-                         type='radio'
-                         checked={field.value === k}
-                         value={k}
-                         onChange={onChangeCb}
-                  />
-                  <label htmlFor={`${field.id}__${k}`}>{field.config.dict[k]}</label>
-                </React.Fragment>
-              )
-            )
-
-             :
-
-            /* non-dictionary */
-            (field.config.type === 'bool' ?
-                <React.Fragment>
-                  <label>Yes <input name={field.id} value={1} checked={!!field.value} onChange={onChangeCb} /></label> :
-                  <label>No <input name={field.id} value={0} checked={!field.value} onChange={onChangeCb} /></label>
-                </React.Fragment> :
-                <React.Fragment>
-                  <input name={field.id} id={field.id} onChange={onChangeCb} value={field.value} type={field.config.type} />
-                </React.Fragment>
-            )
-
-          }
-        </React.Fragment>
-      }</div>)}
-    </form>}
+    {data &&
+      <DataForm
+        fields={config.fields.map(id => ({
+          id,
+          label: id.replace(/([a-z])([A-Z0-9])/g, "$1 $2").replace(/(^| )[a-z]/, s => s.toUpperCase()),
+          ...CamperModel.config[id]
+        }))}
+        data={data}
+        onSubmit={onSave}
+      />}
   </section>
 };
 
