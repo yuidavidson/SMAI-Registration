3f61c11128885529a99ba43c628a2645190f7bf7
Ivan Velev <vanecat@gmail.com>
2022-01-21 14:53:22 -0800

    events: show event sessions details

--- client/src/components/pages/Events.js
+++ client/src/components/pages/Events.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import api from "../../api/api";
 import CampModel from "../../models/camp";
+import SessionModel from "../../models/session";
 import {makeUrl} from "../../utils/nav-utils";
 
 const Events = () => {
@@ -26,15 +27,50 @@ const Events = () => {
       })
   }, []);
 
+  const [eventSessions, setEventSessions] = useState({});
+  const [eventError, setEventError] = useState({});
+  const getSessions = (eventId) => {
+    api.run('event/sessions', {eventId}, true)
+      .then((response) => {
+        let data = response.data.map(item => new SessionModel({
+          ...item,
+          startDate: new Date(item.startDate * 1000).toLocaleDateString(),
+          endDate: new Date(item.endDate * 1000).toLocaleDateString(),
+        }));
+        // console.log({...eventSessions, [eventId]: [...data]});
+        setEventSessions({...eventSessions, [eventId]: [...data]});
+      })
+      .catch((error) => {
+        // INTENTIONALLY NOT COPY PREVIOUS value: {...eventSessionsError, newstuff: {}}
+        // only show last error
+        setEventError({[eventId]: error.toString()});
+        console.log(error);
+      });
+  };
+
+  const onSessionsClickCb = (eventId, e) => {
+    e.preventDefault();
+    getSessions(eventId);
+  };
+
   return <section>
     <h1>Events/Camps</h1>
-    {eventList.map(/** @param {CampModel} event */ (event) =>
-    <div key={event.name}>
+    {eventList.map((/** @param {CampModel} event */ event) =>
+    <div key={event.name} className='event'>
       <h2>{event.name} </h2>
       <div>{event.startDate} - {event.endDate}</div>
       <div>{event.maxQuota} campers max</div>
+      <div className='sessions'>
+        {eventSessions[event.id] ? <h3>Sessions</h3>:<a href='#sessions' onClick={e => onSessionsClickCb(event.id, e)}>Sessions Details</a>}
+        {eventSessions[event.id] && eventSessions[event.id].map(session =>
+          <div>
+            <h4>{session.name}</h4>
+            <div>{session.startDate} - {session.endDate}</div>
+          </div>
+        )}
+        {eventError[event.id] && <span style={{color: 'red'}}>{eventError[event.id]}</span>}
+      </div>
       <Link to={makeUrl('/register/'+event.urlName)}>Register</Link>
-      <br/><br/>
     </div>)}
   </section>
 };
